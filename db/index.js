c85265e849a078168ab73221e103fc2433d744be
Alexander Levin <alevin.ru@gmail.com>
2016-07-19 22:50:36 +0300

    rm db

--- db/index.js
+++
@@ -1,8 +0,0 @@
-/*jslint node: true */
-'use strict';
-
-exports.users = require('./users');
-exports.clients = require('./clients');
-exports.accessTokens = require('./accesstokens');
-exports.authorizationCodes = require('./authorizationcodes');
-exports.refreshTokens = require('./refreshtokens');

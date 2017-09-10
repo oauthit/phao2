aa6937130934d711de87c61bdddc949003521a10
Alexander Levin <alevin.ru@gmail.com>
2017-09-11 00:23:11 +0300

    fix schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,34 +5,35 @@ util.setUserOption 'asamium.default.domain', 'oa2';
 
 meta.defineType 'name:CODE';
 meta.defineType 'secret:STRING';
+
 meta.defineType 'isTrusted:BOOL';
 meta.defineType 'isDisabled:BOOL';
 meta.defineType 'isDeleted:BOOL';
 meta.defineType 'isUsed:BOOL';
+meta.defineType 'isDeleted:BOOL';
+meta.defineType 'isConfirmed:BOOL';
+
 meta.defineType 'code:STRING';
 meta.defineType 'token:STRING';
 meta.defineType 'expiresAt:TS';
 meta.defineType 'attempts:INT,1';
 meta.defineType 'scope:STRING';
-meta.defineType 'mobile_number:STRING';
+meta.defineType 'mobileNumber:CODE';
 meta.defineType 'email:STRING';
 meta.defineType 'status:MEDIUM';
 meta.defineType 'url:STRING';
 meta.defineType 'logoUrl:STRING';
 meta.defineType 'title:STRING';
+meta.defineType 'info:STRING,,nullable';
 
 meta.defineEntity 'Account',
-    ''
+    'name;mobileNumber;info;email;isConfirmed'
 ;
 
 meta.defineEntity 'Client',
     'name;secret;isTrusted;isDisabled;logoUrl;title'
 ;
 
-meta.defineEntity 'Login',
-    'code;expiresAt;attempts,,0;isDeleted',
-    'Client,,nullable;Account;AccessToken,,nullable'
-;
 
 meta.defineEntity 'AuthCode',
     'code;redirectURI,url;scope;isUsed;isDeleted',
@@ -49,17 +50,24 @@ meta.defineEntity 'AccessToken',
     'Client;Account;RefreshToken,,nullable'
 ;
 
---meta.createTable 'Account',0,1;
+meta.defineEntity 'Login',
+    'code;expiresAt;attempts,,0;isDeleted',
+    'Client,,nullable;Account;AccessToken,,nullable'
+;
+
+
+
+meta.createTable 'Account',0,1;
 meta.createTable 'Client',0,1;
 meta.createTable 'Login',0,1;
 meta.createTable 'AuthCode',0,1;
 meta.createTable 'RefreshToken',0,1;
 meta.createTable 'AccessToken',0,1;
 
-alter table oa2.[Login] add foreign key(account) references pha.Agent;
-alter table oa2.AuthCode add foreign key(account) references pha.Agent;
-alter table oa2.RefreshToken add foreign key(account) references pha.Agent;
-alter table oa2.AccessToken add foreign key(account) references pha.Agent;
+--alter table oa2.[Login] add foreign key(account) references pha.Agent;
+--alter table oa2.AuthCode add foreign key(account) references pha.Agent;
+--alter table oa2.RefreshToken add foreign key(account) references pha.Agent;
+--alter table oa2.AccessToken add foreign key(account) references pha.Agent;
 
 alter table oa2.[Login] modify author null;
 alter table oa2.AuthCode modify author null;

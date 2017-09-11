eea2e8f4da906a871e2ff25ab4610fcd1f33b3c9
Alexander Levin <alevin.ru@gmail.com>
2017-09-11 12:57:08 +0300

    lt locale

--- server/config/express.js
+++ server/config/express.js
@@ -8,8 +8,8 @@ var passport = require('passport');
 // internalization
 var i18n = require('i18n');
 i18n.configure({
-  locales: ['en', 'ru'],
-  defaultLocale: 'ru',
+  locales: ['en', 'ru', 'lt'],
+  defaultLocale: config.LANGUAGE,
   directory: __dirname + '/locales',
   register: global
 });
